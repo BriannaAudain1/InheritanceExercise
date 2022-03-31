74739c7e58321b07d78b3e1ef1f7b06b63fcea87
brianna audain <briannaaudain2@outlook.com>
2022-03-31 14:07:58 -0500

    initial commit

---
+++ Inheritance/Bird.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Inheritance
+{
+   public class Bird : Animal 
+    {
+        public string WingColor { get; set; } 
+        public bool CanFly { get; set; }
+        public bool DoMigrate { get; set; }
+        public double BeakLength { get; set; }
+
+       
+
+        public Bird()
+        {
+            IsAlive = true;
+            LegCount = 2;
+            LandSeaAir = "Air";
+            Age = 0;
+        }
+
+
+       
+
+    }
+}
+    
---
+++ Inheritance/Reptile.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Inheritance
+{
+    public class Reptile : Animal 
+    {
+        public Reptile()
+        {
+            IsAlive = true;
+            LegCount = 4;
+            LandSeaAir = "land";
+            Age = 0; 
+        }
+
+        public bool IsColdedBlooded { get; set; }
+        public bool IsScaly { get; set; }
+        public string Habitat { get; set; }
+        public bool CanGrowTail { get; set; }
+    }
+}
